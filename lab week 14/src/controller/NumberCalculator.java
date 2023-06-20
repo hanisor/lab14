a34d3ec45f71d36c737c537e8fb2903944bc644a
hanisor <hanisorhana@gmail.com>
2023-06-21 04:39:21 +0800

    lab week 14 exercise 5 part 1

---
+++ lab week 14/src/controller/NumberCalculator.java
@@ -0,0 +1,39 @@
+package controller;
+
+
+/**
+ * This is a controller class that manage the arithmetic operations.
+ * 
+ * @author hanis sorhana
+ *
+ */
+
+public class NumberCalculator {
+	
+	
+	/**
+	 * A method that will get the summation from two numbers
+	 * @param number1
+	 * @param number2
+	 * @return
+	 */
+	public int getSum(int number1, int number2, int number3) {
+		
+		return number1 + number2 + number3;
+		
+	}
+	
+	/*
+	 *  This method multiplies two numbers
+	 *  @param number1
+	 *  @param number2 
+	 *  @return
+	 *  
+	 */
+	
+	public int getMultiplication (int number1, int number2, int number3) {
+		
+		return number1 * number2 * number3;
+	}
+
+}
